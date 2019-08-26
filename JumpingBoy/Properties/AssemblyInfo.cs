f4373eb9a09722016b8b5fbc3d3157a5bcb4bde8
Gabriel Sant' Ana Pereira <gariel@ymail.com>
2019-08-26 00:26:27 -0300

    readme and screenshots

--- JumpingBoy/Properties/AssemblyInfo.cs
+++ JumpingBoy/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Sweet Jumper")]
-[assembly: AssemblyDescription("Sweet Jumper Game")]
+[assembly: AssemblyTitle("Jumping Boy")]
+[assembly: AssemblyDescription("Jumping Boy Game")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("gariel")]
-[assembly: AssemblyProduct("Sweet Jumper")]
+[assembly: AssemblyProduct("Jumping Boy")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
---
+++ README.md
@@ -0,0 +1,14 @@
+
+# Jumping Boy
+
+Game made aiming a [Weekly Game Jam](https://weeklygamejam.itch.io/), but I couldn't get it finished in time.
+
+Anyway, I used this simple game to create a simple helper library, [LittleCoins](github.com/gariel/LittleCoins)
+
+90% of game logic is in `Game.cs` file and it's not a good code but it works.
+
+- Game Play  
+![](Screenshots/1.png)
+
+- Game Over  
+![](Screenshots/2.png)
